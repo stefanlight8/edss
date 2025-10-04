5a616b33db9f74f174414b105352ffcd11b57995
stefanlight8 <64615032+stefanlight8@users.noreply.github.com>
2025-10-04 18:44:44 +0300

    feat(game-session): add power, total merits and turn all calculated fields from float (`*_per_hour`) to float feat(game-session): rename `*_per_hour` functions

--- packages/edss-backend/src/edss_backend/session.py
+++ packages/edss-backend/src/edss_backend/session.py
@@ -5,12 +5,16 @@ import msgspec
 
 class GameSession(msgspec.Struct):
     """Session in the game.
-    
+
     Each session have own journal and we must read last journal to get session information.
-    
+
     Journal creates → we create session.
     """
+
     started_at: datetime.datetime
+
+    power: str | None = None
+    total_merits: int = 0
 
     deaths: int = 0
     ships_lost: int = 0
@@ -20,24 +24,24 @@ class GameSession(msgspec.Struct):
     ships_destroyed: int = 0
 
     bounties_earned: int = 0
-    bounties_earned_per_hour: int = 0
+    bounties_earned_per_hour: float = 0
     """This is placeholder for calculated value e.g., from `*_per_hour`."""
 
     merits_earned: int = 0
-    merits_earned_per_hour: int = 0
+    merits_earned_per_hour: float = 0
     """This is placeholder for calculated value e.g., from `*_per_hour`."""
 
     combat_bonds_earned: int = 0
-    combat_bonds_earned_per_hour: int = 0
+    combat_bonds_earned_per_hour: float = 0
     """This is placeholder for calculated value e.g., from `*_per_hour`."""
 
     credits_earned: int = 0
     """This is placeholder for calculated value from `get_total_credits_earned`."""
-    credits_earned_per_hour: int = 0
+    credits_earned_per_hour: float = 0
     """This is placeholder for calculated value e.g., from `*_per_hour`."""
 
     def hours_played(self) -> float:
-        now = datetime.datetime.now(datetime.timezone.utc)
+        now = datetime.datetime.now()
         delta = now - self.started_at
         return max(delta.total_seconds() / 3600, 1e-6)
 
@@ -47,13 +51,13 @@ class GameSession(msgspec.Struct):
     def get_kills_per_hour(self) -> float:
         return self.kills / self.hours_played()
 
-    def get_combat_bonds_per_hour(self) -> float:
+    def get_combat_bonds_earned_per_hour(self) -> float:
         return self.combat_bonds_earned / self.hours_played()
 
-    def get_merits_per_hour(self) -> float:
+    def get_merits_earned_per_hour(self) -> float:
         return self.merits_earned / self.hours_played()
 
-    def get_bounties_per_hour(self) -> float:
+    def get_bounties_earned_per_hour(self) -> float:
         return self.bounties_earned / self.hours_played()
 
     def get_total_credits_earned_per_hour(self) -> float:

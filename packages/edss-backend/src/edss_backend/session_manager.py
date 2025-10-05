940ae68810eb027223cf0b78fe77aa971e01869f
stefanlight8 <64615032+stefanlight8@users.noreply.github.com>
2025-10-06 00:08:24 +0300

    feat: move event processing into a separate function

--- packages/edss-backend/src/edss_backend/session_manager.py
+++ packages/edss-backend/src/edss_backend/session_manager.py
@@ -31,6 +31,56 @@ class GameSessionManager:
         self.reader: JournalReader | None = None
         self.game_session: GameSession | None = None
 
+    def process_event(self, event: JournalBaseEvent) -> None:
+        if not self.game_session:
+            return
+        match event:
+            case PowerplayMerits(
+                power=power,
+                merits_gained=merits_gained,
+                total_merits=total_merits,
+            ):
+                self.game_session.power = power
+                self.game_session.merits_earned += merits_gained
+                self.game_session.merits_earned_per_hour = (
+                    self.game_session.get_merits_earned_per_hour()
+                )
+                self.game_session.total_merits = total_merits
+            case Bounty(total_reward=total_reward):
+                self.game_session.bounties_earned += total_reward
+                self.game_session.bounties_earned_per_hour = (
+                    self.game_session.get_bounties_earned_per_hour()
+                )
+                self.game_session.credits_earned = (
+                    self.game_session.get_total_credits_earned()
+                )
+                self.game_session.credits_earned_per_hour = (
+                    self.game_session.get_total_credits_earned_per_hour()
+                )
+                self.game_session.ships_destroyed += 1
+            case CreditsExpense(cost=cost):
+                self.game_session.credits_expenses -= cost
+                self.game_session.credits_earned_per_hour = (
+                    self.game_session.get_total_credits_earned_per_hour()
+                )
+            case NpcCrewPaidWage(amount=amount):
+                self.game_session.credits_expenses -= amount
+                self.game_session.credits_earned_per_hour = (
+                    self.game_session.get_total_credits_earned_per_hour()
+                )
+            case FactionKillBond(reward=reward):
+                self.game_session.combat_bonds_earned += reward
+                self.game_session.combat_bonds_earned_per_hour = (
+                    self.game_session.get_combat_bonds_earned_per_hour()
+                )
+                self.game_session.credits_earned += reward
+                self.game_session.credits_earned_per_hour = (
+                    self.game_session.get_total_credits_earned_per_hour()
+                )
+                self.game_session.ships_destroyed += 1
+            case Died():
+                self.game_session.deaths += 1
+
     async def dispatch_session_update(
         self, journal_file: pathlib.Path | None = None
     ) -> None:
@@ -57,51 +107,13 @@ class GameSessionManager:
                 event: JournalBaseEvent | None = EventFactory.create(raw_event)
                 if event:
                     self._logger.debug("received event %r", event)
-                    if self.game_session:
-                        match event:
-                            case PowerplayMerits(
-                                power=power,
-                                merits_gained=merits_gained,
-                                total_merits=total_merits,
-                            ):
-                                self.game_session.power = power
-                                self.game_session.merits_earned += merits_gained
-                                self.game_session.merits_earned_per_hour = (
-                                    self.game_session.get_merits_earned_per_hour()
-                                )
-                                self.game_session.total_merits = total_merits
-                                await self.dispatch_session_update()
-                            case Bounty(total_reward=total_reward):
-                                self.game_session.bounties_earned += total_reward
-                                self.game_session.bounties_earned_per_hour = (
-                                    self.game_session.get_bounties_earned_per_hour()
-                                )
-                                self.game_session.credits_earned = (
-                                    self.game_session.get_total_credits_earned()
-                                )
-                                self.game_session.credits_earned_per_hour = self.game_session.get_total_credits_earned_per_hour()
-                                self.game_session.ships_destroyed += 1
-                                await self.dispatch_session_update()
-                            case CreditsExpense(cost=cost):
-                                self.game_session.credits_earned -= cost
-                                self.game_session.credits_earned_per_hour = self.game_session.get_total_credits_earned_per_hour()
-                                await self.dispatch_session_update()
-                            case NpcCrewPaidWage(amount=amount):
-                                self.game_session.credits_earned -= amount
-                                self.game_session.credits_earned_per_hour = self.game_session.get_total_credits_earned_per_hour()
-                                await self.dispatch_session_update()
-                            case FactionKillBond(reward=reward):
-                                self.game_session.combat_bonds_earned += reward
-                                self.game_session.combat_bonds_earned_per_hour = (
-                                    self.game_session.get_combat_bonds_earned_per_hour()
-                                )
-                                self.game_session.credits_earned += amount
-                                self.game_session.credits_earned_per_hour = self.game_session.get_total_credits_earned_per_hour()
-                                self.game_session.ships_destroyed += 1
-                                await self.dispatch_session_update()
-                            case Died():
-                                self.game_session.deaths += 1
-                                await self.dispatch_session_update()
+                    try:
+                        self.process_event(event)
+                    except Exception as exc:
+                        self._logger.error(
+                            "failed to process event: %s", exc, exc_info=exc
+                        )
+                    await self.dispatch_session_update()
 
     async def start(self) -> None:
         self.reader = JournalReader(self.folder_path)
